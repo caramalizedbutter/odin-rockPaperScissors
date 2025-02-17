3bc5eda5aa7b653a85c840df532d47e46a7a5691
caramalizedbutter <prince08bkb@gmail.com>
2025-02-17 13:20:25 +0530

    Added a final winner declaration

--- script.js
+++ script.js
@@ -46,9 +46,19 @@ function playRound(){
 
 function playGame(){
     for(let i=0;i<5;i++){
+        console.log(`Round ${i+1} : `);
         playRound();
     }
     console.log(`Final Score : You -> ${playerScore} , Computer -> ${compScore} , Draw -> ${draw}`);
+    if(compScore===playerScore){
+        console.log("IT'S A DRAW!!!");
+    }
+    else if(playerScore>compScore){
+        console.log("YOU WIN!!!");
+    }
+    else{
+        console.log("COMPUTER WINS!!!");
+    }
 }
 
 playGame();
