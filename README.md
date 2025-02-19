8c0722c5b592a6e13d74171845e89982dac4734c
caramalizedbutter <prince08bkb@gmail.com>
2025-02-19 21:11:43 +0530

    Added buttons and a simple GUI for now

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # odin-rockPaperScissors
 Rock-Paper-Scissor Project from The Odin Project
+
+// for(let i=0;i<5;i++){
+    //     console.log(`Round ${i+1} : `);
+    //     playRound();
+    // }
+    // console.log(`Final Score : You -> ${playerScore} , Computer -> ${compScore} , Draw -> ${draw}`);
+    // if(compScore===playerScore){
+    //     console.log("IT'S A DRAW!!!");
+    // }
+    // else if(playerScore>compScore){
+    //     console.log("YOU WIN!!!");
+    // }
+    // else{
+    //     console.log("COMPUTER WINS!!!");
+    // }
--- script.js
+++ script.js
@@ -1,64 +1,76 @@
-let choice=["rock", "paper", "scissors"], playerScore=0, compScore=0, draw=0;
+let choice = ["rock", "paper", "scissors"], playerScore = 0, compScore = 0, draw = 0, roundCount = 0;
 
-function getComputerChoice(){
-    let compChoice=Math.floor(Math.random()*3);
+//All UI Elements are created globally
+const logBox = document.createElement("div");
+const body = document.querySelector("body");
+logBox.setAttribute("id", "logBox");
+body.appendChild(logBox);
+const logText = document.createElement("div");
+const logScore = document.createElement("div");
+const logPlayerScore = document.createElement("div");
+const logCompScore = document.createElement("div");
+logText.setAttribute("id", "logText");
+logScore.setAttribute("id", "logScore");
+logPlayerScore.setAttribute("id", "logPlayerScore");
+logCompScore.setAttribute("id", "logCompScore");
+logBox.append(logText, logScore);
+logScore.append(logPlayerScore, logCompScore);
+logText.textContent = `Start the game!!!`;
+logCompScore.textContent = `Computer Score : ${compScore}`;
+logPlayerScore.textContent = `Player Score : ${playerScore}`;
+
+function getComputerChoice() {
+    let compChoice = Math.floor(Math.random() * 3);
     console.log(`Computer chose ${choice[compChoice]}`);
     return choice[compChoice];
 }
 
-function getHumanChoice(){
-    let x = prompt("Enter your guess : (rock, paper, scissors)");
-    x=x.toLowerCase();
-    if(choice.includes(x)){
-        console.log(`You chose ${x}`);
-        return x;
-    }
-    else{
-        console.log("Invalid choice! Try again...");
-        return getHumanChoice();
-    }
-}
-
-function playRound(){
-    let humanChoice=getHumanChoice();
-    let computerChoice=getComputerChoice();
-    if(humanChoice === computerChoice){
-        draw+=1;
+function playRound(humanChoice) {
+    let computerChoice = getComputerChoice();
+    roundCount += 1;
+    if (humanChoice === computerChoice) {
+        draw += 1;
         console.log(`It's a draw! Both chose ${humanChoice}...`);
+        logText.textContent = `It's a DRAW!!!`;
+        if (roundCount <= 4) {
+            logText.textContent = `Round ${roundCount} : DRAW`;
+        }
     }
-    else if(humanChoice=="rock" && computerChoice=="paper"){
-        compScore+=1;
-        console.log("Paper beats rock! Computer wins...");
+    else if ((humanChoice == "rock" && computerChoice == "paper") || (humanChoice == "paper" && computerChoice == "scissors") || (humanChoice == "scissors" && computerChoice == "rock")) {
+        compScore += 1;
+        logCompScore.textContent = `Computer Score : ${compScore}`;
+        if (roundCount <= 4) {
+            logText.textContent = `Round ${roundCount} : Computer wins...`;
+        }
     }
-    else if(humanChoice=="paper" && computerChoice=="scissor"){
-        compScore+=1;
-        console.log("Scissors beats paper! Computer wins...");
-    }
-    else if(humanChoice=="scissor" && computerChoice=="rock"){
-        compScore+=1;
-        console.log("Rock beats scissors! Computer wins");
-    }
-    else{
-        playerScore+=1;
+    else {
+        playerScore += 1;
         console.log(`${humanChoice} beats ${computerChoice}! You win...`);
+        logPlayerScore.textContent = `Player Score : ${playerScore}`;
+        if (roundCount <= 4) {
+            logText.textContent = `Round ${roundCount} : You win!!!`;
+        }
+    }
+    if (roundCount >= 5) {
+        document.querySelectorAll("button").forEach((button) => button.disabled = true);
+        if (playerScore > compScore) {
+            logText.textContent = "You won the game!!!";
+        }
+        else if (compScore > playerScore) {
+            logText.textContent = "Oh no! Computer won the game.";
+        }
+        else {
+            logText.textContent = "Nobody won, It's a draw."
+        }
     }
 }
 
-function playGame(){
-    for(let i=0;i<5;i++){
-        console.log(`Round ${i+1} : `);
-        playRound();
-    }
-    console.log(`Final Score : You -> ${playerScore} , Computer -> ${compScore} , Draw -> ${draw}`);
-    if(compScore===playerScore){
-        console.log("IT'S A DRAW!!!");
-    }
-    else if(playerScore>compScore){
-        console.log("YOU WIN!!!");
-    }
-    else{
-        console.log("COMPUTER WINS!!!");
-    }
+function playGame() {
+    document.querySelectorAll("button").forEach((buttn) => {
+        buttn.addEventListener("click", () => {
+            playRound(buttn.id)
+        });
+    });
 }
 
 playGame();
